0de11762d9c0a77a4c52ca69fee1f404f8fb499d
Martin Ballester <mballester@q4tech.com>
2023-10-10 19:21:04 -0300

    add sales

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { getProduct, getProducts, createProduct, deleteProduct } = require('../controllers/productsController');
 const { getClient, getClients, createClient, deleteClient } = require('../controllers/clientsController');
+const { getSales } = require('../controllers/salesController');
 const router = express.Router();
 
 router.get('/getProduct/:id', getProduct);
@@ -12,5 +13,7 @@ router.get('/getClient/:id', getClient);
 router.get('/getClients', getClients);
 router.post('/addClient', createClient);
 router.delete('/deleteClient/:id', deleteClient);
+
+router.get('/getSales', getSales);
 
 module.exports = router
