8c654e49c1beefc56cc84dab6d65372ac5c6ce28
Martin Ballester <martinballester1@gmail.com>
2023-10-10 16:23:25 -0300

    clients controller done

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -1,7 +1,18 @@
+const clientData = fs.readFileSync('./data/clients.json', 'utf-8');
+const clientJSON = JSON.parse(clientData);
+const fs = require('fs');
+
 const Client = require("../models/product");
 
 exports.getClient = (req, res) => {
+    const clientId = req.params.id;
+    const client = clientJSON.find((p) => p.id === parseInt(clientId));
 
+    if (!client) {
+        return res.status(404).json({ message: 'Cliente no encontrado' });
+    }
+
+    res.json(cliente);
 };
 
 exports.getClients = (req, res) => {
